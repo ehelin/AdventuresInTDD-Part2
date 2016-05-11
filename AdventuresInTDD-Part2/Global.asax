44881300c62b7cc255885979d1f22243baa4491f
Eric Helin <ehelin@felpsis.net>
2016-05-11 09:53:27 -0500

    Initial showing requirements

---
+++ AdventuresInTDD-Part2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AdventuresInTDD_Part2.MvcApplication" Language="C#" %>
---
+++ AdventuresInTDD-Part2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AdventuresInTDD_Part2.Startup))]
+namespace AdventuresInTDD_Part2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
